192b2a54d066117911891d74c4392f40cd615014
George Udonte <georgeudonte@gmail.com>
2023-12-02 17:55:27 +0100

    added rating-select component

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -1,42 +1,48 @@
 <script>
-  export let style="primary"
-  export let type="button"
-  export let disabled=false
+  export let style = 'primary'
+  export let type = 'button'
+  export let disabled = false
 </script>
 
-<button type={type} {disabled} class={style}>
+<button {type} {disabled} class={style}>
   <slot></slot>
 </button>
 
+
 <style>
-  button{
-    color: #fff;
+  button {
+    color: gainsboro;
     border: 0;
-    border-radius: 8px;
     width: 100px;
+    height: 40px;
+    cursor: pointer;
+    font-weight: bold;
+    border-top-right-radius: 5px;
+    border-bottom-right-radius: 5px;
   }
 
-  .primary{
+  .primary {
     background-color: #202142;
   }
 
-  .secondary{
-    background-color: #ff6a95;
+  .secondary {
+    background: #ff6a95;
   }
 
-  button:hover{
+  button:hover {
     transform: scale(0.98);
     opacity: 0.9;
   }
 
-  button.disabled{
-    background-color: #ccc;
+  button:disabled{
+    background-color: #cccccc;
     color: #333;
-    cursor: auto;
+    cursor:auto;
   }
 
   button:disabled:hover{
-    transform: scale(1);
+    transform:scale(1);
     opacity: 1;
   }
+
 </style>
--- src/components/FeedbackForm.svelte
+++ src/components/FeedbackForm.svelte
@@ -1,14 +1,86 @@
 <script>
   import Card from "./Card.svelte";
   import Button from "./Button.svelte";
+  import RatingSelect from "./RatingSelect.svelte";
 
+  let btnDisabled = false;
+  let text = "";
+  let min = 10;
+  let message = "";
+  let rating = 10;
+
+  const handleSelect = e => rating = e.detail;
+
+  const handleInput = () => {
+    if(text.trim().length <= min){
+      message = `Text must be atleast ${min} characters`
+      btnDisabled = true;
+    } else {
+      message = null;
+      btnDisabled = false;
+    }
+  }
 </script>
 
-<form >
-  <!-- Rating Select -->
+<Card>
+  <header>
+    <h2>How would you rate your service with us?</h2>
+  </header>
+  
+  <RatingSelect on:rating-select={handleSelect} />
+  <form >
+    <!-- Rating Select -->
+    
+    <div class="input-group">
+      <input on:input={handleInput} type="text" bind:value={text} placeholder="Tell us something that keeps you coming back..">
+      <Button disabled={btnDisabled} type="submit">Send</Button>
+    </div>
 
-  <div class="input-group">
-    <input type="text" placeholder="Tell us something that keeps you coming">
-    <Button></Button>
-  </div>
-</form>
+    {#if message}
+    <div class="message">
+      {message}
+    </div>
+    {/if}
+  </form>
+</Card> 
+
+<style>
+  header {
+    max-width: 400px;
+    margin: auto;
+  }
+
+  header h2 {
+    font-size: 22px;
+    font-weight: 600;
+    text-align: center;
+  }
+
+  .input-group {
+    display: flex;
+    flex-direction: row;
+    padding: 8px 10px; 
+    border-radius: 8px;
+    margin-top: 15px;
+  }
+
+  input {
+    flex-grow: 2;
+    border: none;
+    font-size: 16px;
+    padding: 10px 15px;
+    border-top-left-radius: 5px;
+    border-bottom-left-radius: 5px;
+  }
+
+  input:focus {
+    outline: none;
+  }
+
+  .message{
+    padding-top: 7px;
+    text-align: center;
+    color: black;
+    font-size: 12px;
+  }
+</style>
